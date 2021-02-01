550933800dfa388dd147af8d2a48aaae9dba9795
mignonnesaurus <eli@mignonnesaurus.com>
2021-02-01 22:28:23 +0100

    Remove nested function

--- loose_change_kata.py
+++ loose_change_kata.py
@@ -3,7 +3,9 @@ import math
 
 # Ref: https://www.codewars.com/kata/5571f712ddf00b54420000ee/train/python
 def loose_change(amount_in_cents):
-    def calculate_coins():
+    if amount_in_cents <= 0:
+        return {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
+    else:
         int_amount_in_cents = math.floor(amount_in_cents)
         quarters, mod_quarters = divmod(int_amount_in_cents, 25)
         dimes, mod_dimes = divmod(mod_quarters, 10)
@@ -11,8 +13,3 @@ def loose_change(amount_in_cents):
         pennies = mod_nickels
 
         return {'Nickels': nickels, 'Pennies': pennies, 'Dimes': dimes, 'Quarters': quarters}
-
-    if amount_in_cents <= 0:
-        return {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
-    else:
-        return calculate_coins()
