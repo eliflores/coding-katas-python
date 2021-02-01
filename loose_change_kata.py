a86840c0cbd2484a23a847f1a430fc236eced7ac
mignonnesaurus <eli@mignonnesaurus.com>
2021-02-01 20:47:38 +0100

    Complete Kata :tada:

--- loose_change_kata.py
+++ loose_change_kata.py
@@ -1,4 +1,18 @@
-# Ref: https://www.codewars.com/kata/5571f712ddf00b54420000ee/train/python
+import math
 
+
+# Ref: https://www.codewars.com/kata/5571f712ddf00b54420000ee/train/python
 def loose_change(amount_in_cents):
-    return {}
+    def calculate_coins():
+        int_amount_in_cents = math.floor(amount_in_cents)
+        quarters, mod_quarters = divmod(int_amount_in_cents, 25)
+        dimes, mod_dimes = divmod(mod_quarters, 10)
+        nickels, mod_nickels = divmod(mod_dimes, 5)
+        pennies = mod_nickels
+
+        return {'Nickels': nickels, 'Pennies': pennies, 'Dimes': dimes, 'Quarters': quarters}
+
+    if amount_in_cents <= 0:
+        return {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
+    else:
+        return calculate_coins()
--- tests/loose_change_kata_test.py
+++ tests/loose_change_kata_test.py
@@ -1,7 +1,22 @@
 from loose_change_kata import loose_change
 
 
-def test_loose_change():
-    assert loose_change(56) == {'Nickels': 1, 'Pennies': 1, 'Dimes': 0, 'Quarters': 2}
+def test_returns_all_coins_equal_to_0_when_given_an_amount_equal_to_0():
+    assert loose_change(0) == {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
+
+
+def test_returns_all_coins_equal_to_0_when_given_a_negative_amount():
     assert loose_change(-435) == {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
+    assert loose_change(-2) == {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
+
+
+def test_rounds_the_amount_down_when_the_given_amount_is_a_float():
     assert loose_change(4.935) == {'Nickels': 0, 'Pennies': 4, 'Dimes': 0, 'Quarters': 0}
+    assert loose_change(3.9) == {'Nickels': 0, 'Pennies': 3, 'Dimes': 0, 'Quarters': 0}
+
+
+def test_loose_change_returns_the_least_amount_of_coins():
+    assert loose_change(56) == {'Nickels': 1, 'Pennies': 1, 'Dimes': 0, 'Quarters': 2}
+    assert loose_change(29) == {'Nickels': 0, 'Pennies': 4, 'Dimes': 0, 'Quarters': 1}
+    assert loose_change(91) == {'Nickels': 1, 'Pennies': 1, 'Dimes': 1, 'Quarters': 3}
+
