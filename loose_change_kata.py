40cc40ba2decc77fde9109c545d37a4b92575735
mignonnesaurus <eli@mignonnesaurus.com>
2021-02-01 22:35:12 +0100

    Rename amount_in_cents to cents
    Keeping it simpler.

--- loose_change_kata.py
+++ loose_change_kata.py
@@ -2,12 +2,12 @@ import math
 
 
 # Ref: https://www.codewars.com/kata/5571f712ddf00b54420000ee/train/python
-def loose_change(amount_in_cents):
-    if amount_in_cents <= 0:
+def loose_change(cents):
+    if cents <= 0:
         return {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
     else:
-        int_amount_in_cents = math.floor(amount_in_cents)
-        quarters, mod_quarters = divmod(int_amount_in_cents, 25)
+        cents = math.floor(cents)
+        quarters, mod_quarters = divmod(cents, 25)
         dimes, mod_dimes = divmod(mod_quarters, 10)
         nickels, mod_nickels = divmod(mod_dimes, 5)
         pennies = mod_nickels
