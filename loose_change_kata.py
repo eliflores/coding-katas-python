dcfbb4529e57a7a517fc26b587aca361586a98aa
mignonnesaurus <eli@mignonnesaurus.com>
2021-02-02 09:12:39 +0100

    Fix linting messages

--- loose_change_kata.py
+++ loose_change_kata.py
@@ -1,15 +1,19 @@
+"""Codewars Loose Change Kata"""
+
 import math
 
 
-# Ref: https://www.codewars.com/kata/5571f712ddf00b54420000ee/train/python
 def loose_change(cents):
+    """This function is the solution to the Codewars Loose Change Kata that
+    can be found here https://www.codewars.com/kata/5571f712ddf00b54420000ee/train/python."""
+
     if cents <= 0:
         return {'Nickels': 0, 'Pennies': 0, 'Dimes': 0, 'Quarters': 0}
-    else:
-        cents = math.floor(cents)
-        quarters, mod_quarters = divmod(cents, 25)
-        dimes, mod_dimes = divmod(mod_quarters, 10)
-        nickels, mod_nickels = divmod(mod_dimes, 5)
-        pennies = mod_nickels
 
-        return {'Nickels': nickels, 'Pennies': pennies, 'Dimes': dimes, 'Quarters': quarters}
+    cents = math.floor(cents)
+    quarters, mod_quarters = divmod(cents, 25)
+    dimes, mod_dimes = divmod(mod_quarters, 10)
+    nickels, mod_nickels = divmod(mod_dimes, 5)
+    pennies = mod_nickels
+
+    return {'Nickels': nickels, 'Pennies': pennies, 'Dimes': dimes, 'Quarters': quarters}
