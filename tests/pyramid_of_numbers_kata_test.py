eac33f77787b367a0d7aec8453a29356d78d2330
Eli Flores <eli@mignonnesaurus.com>
2021-02-24 23:09:13 +0100

    Pyramid kata (#7)
    Also moved katas to their own package.

---
+++ tests/pyramid_of_numbers_kata_test.py
@@ -0,0 +1,13 @@
+from katas.pyramid_of_numbers_kata import levels_content
+
+
+def test_returns_an_list_string_when_zero_is_given_as_input():
+    assert levels_content(0) == []
+
+
+def test_returns_the_levels_of_a_pyramid_of_numbers():
+    assert levels_content(1) == ['1']
+    assert levels_content(2) == ['1', '1 2 1']
+    assert levels_content(3) == ['1', '1 2 1', '1 2 3 2 1']
+    assert levels_content(4) == ['1', '1 2 1', '1 2 3 2 1', '1 2 3 4 3 2 1']
+    assert levels_content(5) == ['1', '1 2 1', '1 2 3 2 1', '1 2 3 4 3 2 1', '1 2 3 4 5 4 3 2 1']
