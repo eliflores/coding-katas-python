d8faf6db88b5366788afface54975ab1ac8315ab
Eli Flores <eli@mignonnesaurus.com>
2021-03-30 21:20:33 +0200

    Duplicate Encoder Kata (#9)

---
+++ katas/duplicate_encoder.py
@@ -0,0 +1,22 @@
+"""Codewars Duplicate Encoder Kata"""
+
+
+def duplicate_encode(word):
+    """This function is the solution to the Codewars Duplicate Encoder Kata that
+        can be found here https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/python."""
+
+    word_lowercase = word.lower()
+    occurrences = {}
+    for character in list(word_lowercase):
+        number_of_times = occurrences.get(character, 0)
+        number_of_times += 1
+        occurrences[character] = number_of_times
+
+    encoded_string = []
+    for character in word_lowercase:
+        if occurrences[character] == 1:
+            encoded_string.append('(')
+        else:
+            encoded_string.append(')')
+
+    return ''.join(encoded_string)
---
+++ tests/duplicate_encoder_test.py
@@ -0,0 +1,8 @@
+from katas.duplicate_encoder import duplicate_encode
+
+
+def test_duplicate_encode():
+    assert duplicate_encode("din") == "((("
+    assert duplicate_encode("recede") == "()()()"
+    assert duplicate_encode("Success") == ")())())"
+    assert duplicate_encode("(( @") == "))(("
