23c2245760c65104c049669b5e3702239b5a0c3d
mignonnesaurus <eli@mignonnesaurus.com>
2021-04-13 20:31:47 +0200

    Solution to the Sum of parts Kata
    This one passes the performance tests :raised_hands:

--- katas/sum_of_parts.py
+++ katas/sum_of_parts.py
@@ -1,16 +1,17 @@
 """Codewars Sums of Parts Kata"""
 
 
-def parts_sums(numbers):
+def parts_sums(ls):
     """This function is the solution to the Codewars Loose Change Kata that
     can be found here https://www.codewars.com/kata/5ce399e0047a45001c853c2b/train/python."""
-    ls_len = len(numbers)
-    if ls_len == 0:
+    list_length = len(ls)
+    if list_length == 0:
         return [0]
 
-    sums = [sum(numbers)]
-    for _ in range(ls_len):
-        numbers.pop(0)
-        sums.append(sum(numbers))
+    last_sum = sum(ls)
+    sums = [last_sum]
+    for _ in range(list_length):
+        last_sum = last_sum - ls.pop(0)
+        sums.append(last_sum)
 
     return sums
