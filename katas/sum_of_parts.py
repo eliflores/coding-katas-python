2c44e306a58c99e3ef3ff9fce414916e8a0310fc
mignonnesaurus <eli@mignonnesaurus.com>
2021-04-13 21:15:42 +0200

    Sum of Parts Kata Update
    We do not need the empty list check.

--- katas/sum_of_parts.py
+++ katas/sum_of_parts.py
@@ -5,9 +5,6 @@ def parts_sums(ls):
     """This function is the solution to the Codewars Loose Change Kata that
     can be found here https://www.codewars.com/kata/5ce399e0047a45001c853c2b/train/python."""
     list_length = len(ls)
-    if list_length == 0:
-        return [0]
-
     last_sum = sum(ls)
     sums = [last_sum]
     for _ in range(list_length):
