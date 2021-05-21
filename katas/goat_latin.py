f96b4b5c6a42f525b77d1fc48077e11aaddc8f7c
mignonnesaurus <eli@mignonnesaurus.com>
2021-05-21 22:01:59 +0200

    Goat Latin Kata
    TODO: Implement! :)

---
+++ katas/goat_latin.py
@@ -0,0 +1,9 @@
+"""LeetCode Goat Lating Kata
+From: https://leetcode.com/problems/goat-latin/"""
+
+
+def to_goat_latin(sentence):
+    """A sentence sentence is given, composed of words separated by spaces. Each word consists of
+    lowercase and uppercase letters only.
+    We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)"""
+    return ''
