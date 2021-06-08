05a4cb6b63326c206b35675f9650e3e84042e189
Eli Flores <eli@mignonnesaurus.com>
2021-06-08 22:48:42 +0200

    Rename square_in_rectangle to squares_in_rectangle (#14)

--- tests/rectangle_into_squares_test.py
+++ tests/rectangle_into_squares_test.py
@@ -1,11 +1,11 @@
-from katas.rectangle_into_squares import square_in_rectangle
+from katas.rectangle_into_squares import squares_in_rectangle
 
 
 def test_square_in_rectangles_when_length_and_width_are_equal():
-    assert square_in_rectangle(5, 5) is None
+    assert squares_in_rectangle(5, 5) is None
 
 
 def test_square_in_rectangles():
-    assert square_in_rectangle(5, 3) == [3, 2, 1, 1]
-    assert square_in_rectangle(20, 14) == [14, 6, 6, 2, 2, 2]
-    assert square_in_rectangle(37, 14) == [14, 14, 9, 5, 4, 1, 1, 1, 1]
+    assert squares_in_rectangle(5, 3) == [3, 2, 1, 1]
+    assert squares_in_rectangle(20, 14) == [14, 6, 6, 2, 2, 2]
+    assert squares_in_rectangle(37, 14) == [14, 14, 9, 5, 4, 1, 1, 1, 1]
