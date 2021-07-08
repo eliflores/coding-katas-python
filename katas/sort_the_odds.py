123df772c389274cbbd4db9fe19f691c457913b4
Eli Flores <eli@mignonnesaurus.com>
2021-07-08 15:29:52 +0200

    Updates to sort the odd Kata  (#17)
    * Use pop(n) instead of an iterator.
    * Use list comprehensions.

--- katas/sort_the_odds.py
+++ katas/sort_the_odds.py
@@ -5,14 +5,6 @@ def sort_array(source_array):
     """This function is the solution to the Codewars Sort the odd Kata that
         can be found here https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/python."""
 
-    odds = list(filter(lambda n: n % 2 == 1, source_array))
+    odds = [n for n in source_array if n % 2 == 1]
     sorted_odds = sorted(odds)
-    sorted_odds_iter = iter(sorted_odds)
-    dest_array = []
-    for number in source_array:
-        if number % 2 == 0:
-            dest_array.append(number)
-        else:
-            dest_array.append(next(sorted_odds_iter))
-
-    return dest_array
+    return [n if n % 2 == 0 else sorted_odds.pop(0) for n in source_array]
