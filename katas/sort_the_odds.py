e93de025a58b77b6a338b51792e5ff0dd262275c
mignonnesaurus <eli@mignonnesaurus.com>
2021-07-08 15:13:43 +0200

    Use list comprehensions
    Python <3

--- katas/sort_the_odds.py
+++ katas/sort_the_odds.py
@@ -5,13 +5,6 @@ def sort_array(source_array):
     """This function is the solution to the Codewars Sort the odd Kata that
         can be found here https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/python."""
 
-    odds = list(filter(lambda n: n % 2 == 1, source_array))
+    odds = [n for n in source_array if n % 2 == 1]
     sorted_odds = sorted(odds)
-    dest_array = []
-    for number in source_array:
-        if number % 2 == 0:
-            dest_array.append(number)
-        else:
-            dest_array.append(sorted_odds.pop(0))
-
-    return dest_array
+    return [n if n % 2 == 0 else sorted_odds.pop(0) for n in source_array]
